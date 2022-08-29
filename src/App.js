22b1106a58a628ad7162842c0dba4a84c31a3ac1
MohdUmar <Mohdumar8300@gmail.com>
2022-08-30 03:00:12 +0530

    after pulling files

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React, { Component } from 'react';
 import './App.css';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import News from './components/News';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
-
+const routes = [
+  { path: '/', category: 'general', country: 'in' },
+  { path: '/business', category: 'business', country: 'in' },
+  { path: '/entertainment', category: 'entertainment', country: 'in' },
+  { path: '/health', category: 'health', country: 'in' },
+  { path: '/science', category: 'science', country: 'in' },
+  { path: '/sports', category: 'sports', country: 'in' },
+  { path: '/technology', category: 'technology', country: 'in' },
+];
 
 export default class App extends Component {
+  /**
+   *  Your react-router wasn't installed
+   *  I'm assuming you install react-router-dom as dev dependency
+   *  Always map components if only values are changing, this will help you in a long run
+   *  I used one array of data in two different places, now you don't have to change two files.
+   */
+
   render() {
     return (
       <BrowserRouter>
-        <News/>
-        
-        <Navbar/>
+        <Navbar routes={routes} />
 
-        <Routes>  
-          <Route path="/" element={<News country="in" category="general" pagesize={10}/>} />
-          <Route path="/business" element={<News country="in" category="business" pagesize={10}/>} />
-          <Route path="/entertainment" element={<News country="in" category="entertainment" pagesize={10}/>} />
-          <Route path="/general" element={<News country="in" category="general" pagesize={10}/>} />
-          <Route path="/health" element={<News country="in" category="health" pagesize={10}/>} />
-          <Route path="/science" element={<News country="in" category="science" pagesize={10}/>} />
-          <Route path="/sports" element={<News country="in" category="sports" pagesize={10}/>} />
+        <Routes>
+          {routes.map(({ path, category, country }) => (
+            <Route
+              key={category}
+              path={path}
+              element={<News country={country} category={category} pagesize={10} />}
+            />
+          ))}
         </Routes>
-
       </BrowserRouter>
-    )
+    );
   }
 }
-
-
-
-
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+
+// Always use NavLink instead of Link read this https://v5.reactrouter.com/web/api/NavLink
+import { NavLink as Link } from 'react-router-dom';
 
 export class Navbar extends Component {
   render() {
+    const { routes } = this.props;
+
     return (
       <div>
         <nav className="navbar navbar-expand-lg bg-light">
@@ -21,52 +25,17 @@ export class Navbar extends Component {
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              
               <ul className="navbar-nav me-auto mb-2 mb-lg-0 gap-4">
-                
-                <li className="nav-item">
-                  <Link className="nav-Link active" to="/">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-Link" to="/business">
-                    Business
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-Link" to="/entertainment">
-                    Entertainment
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-Link" to="/generel">
-                    Generel
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-Link" to="/health">
-                    Health
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-Link" to="/science">
-                    Science
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-Link" to="/sports">
-                    Sports
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-Link" to="/technology">
-                    Technology
-                  </Link>
-                </li>
+                {routes.map(({ path, category }) => (
+                  <li key={path} className="nav-item">
+                    <Link className="nav-Link" activeClassName="active" to={path}>
+                      {path === '/' ? 'Home' : category}
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
